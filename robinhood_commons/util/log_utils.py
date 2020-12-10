1e0b72bb652009160ba2a37af480ff2f89d019f9
Matt Howell <mhowell234@gmail.com>
2020-12-10 18:27:55 -0500

    Fix logger creation name

--- robinhood_commons/util/log_utils.py
+++ robinhood_commons/util/log_utils.py
@@ -6,18 +6,18 @@ from log.gzip_rotator import GZipRotator
 from robinhood_commons.util.io_utils import ensure_exists
 
 
-def create_logger(name: str) -> Logger:
+def create_logger(path: str) -> Logger:
     """
     Creates a rotating log
     """
 
-    ensure_exists(name)
+    ensure_exists(path)
 
-    log_name: str = name.split('/')[-1]
+    log_name: str = path.split('/')[-1]
     logger = logging.getLogger(log_name.split('.')[0])
     logger.setLevel(logging.INFO)
 
-    log_handler = TimedRotatingFileHandler(filename=name, when='h')
+    log_handler = TimedRotatingFileHandler(filename=path, when='h')
 
     log_formatter = logging.Formatter('%(asctime)s [%(levelname)s] [%(name)s] %(message)s')
     log_handler.setFormatter(log_formatter)
